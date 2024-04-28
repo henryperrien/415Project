a0eb1a7a07898e0746df80cd0d31d406df5cba01
henryperrien <henry.perrien@selu.edu>
2024-04-28 13:00:52 -0500

    fix routing

--- app.js
+++ app.js
@@ -3,32 +3,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const database = require('./database');
-const UserController = require('./Controllers/UserController');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const defaultRoute = require('./Routes/index');
 const userRoutes = require('./Routes/userRouter');
+const messageRoutes = require('./Routes/messageRouter');
 
 app.use('/', defaultRoute);
 app.use('/user', userRoutes);
-app.post('/login', async (req, res) => {
-    try {
-        await UserController.login(req, res);
-    } catch (error) {
-        res.status(500).json({ message: 'Error logging in', error: error.message });
-    }
-});
-
-app.post('/register', async (req, res) => {
-    try {
-        await UserController.register(req, res);
-    } catch (error) {
-        res.status(500).json({ message: 'Error registering user', error: error.message });
-    }
-});
-
+app.use('/msgs', messageRoutes);
 
 
 
