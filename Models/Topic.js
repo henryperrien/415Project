9ce2239f9f469276efa9f84438889d3b34b69dd7
Quade Witt <wittquadeallen@gmail.com>
2024-04-28 23:33:51 -0500

    applied observer pattern on user end

--- Models/Topic.js
+++ Models/Topic.js
@@ -5,6 +5,7 @@ class Topic {
         this.name = name;
         this.subscribers = [];
     }
+    
 
     async save() {
         const db = database.getDB();
@@ -22,6 +23,23 @@ class Topic {
                  .find()
                  .toArray();
     }
+
+    async subscribe(User) {
+        const db = database.getDB();
+        await db.collection('UserCollection415').updateOne(
+            { topicId: this.topicId },
+            { $addToSet: { subscribers: username } }
+        );
+    }
+
+    async unsubscribe(User) {
+        const db = database.getDB();
+        await db.collection('UserCollection415').updateOne(
+            { username: this.topicId },
+            { $pull: { subscribers: username } }
+        );
+    }
+
 }
 
 module.exports = Topic;
